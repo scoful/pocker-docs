e603da5163a820c0f3a2adbdcbd6a3b78426afa2
scoful <1269717999@qq.com>
2025-02-26 23:23:22 +0800

    init

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,13 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Pocker 使用文档</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/scoful/pocker',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/scoful/pocker-docs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Pocker - 让每个人都有自己的 Docker 私服，Make Docker Great Again -  ❤ by scoful',
   },
 }
 
